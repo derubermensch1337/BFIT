486a37fae278630ec81f137acf5a1c45431cd929
s234384 <155777444+s234384@users.noreply.github.com>
2026-01-14 14:15:52 +0100

    Update style_css.h
    Added documentation

--- include/style_css.h
+++ include/style_css.h
@@ -1,3 +1,18 @@
+/**
+ * @file style_css.h
+ * @authors Baldur G. Toftegaard
+ * @brief 
+ * @version 0.1
+ * @date 13-01-2026
+ * @par Revision history
+ * | Version |    Date    | Description                                                             |
+ * |---------|------------|-------------------------------------------------------------------------|
+ * | 0.1     | 13-01-2026 | Creaded file, implemented start and admin page, started general design  |
+ * | 0.1     |            |                                                                         |
+ * 
+ * @copyright Copyright (c) 2026
+*/
+
 #ifndef STYLE_CSS_H
 #define STYLE_CSS_H
 
