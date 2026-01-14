067543a4ffbe3dd383ac507217123b749d8d454f
s234384 <155777444+s234384@users.noreply.github.com>
2026-01-14 14:17:28 +0100

    Update login_html.h
    Added documentation

--- include/login_html.h
+++ include/login_html.h
@@ -1,3 +1,18 @@
+/**
+ * @file login_html.h
+ * @authors Baldur G. Toftegaard
+ * @brief 
+ * @version 0.1
+ * @date 13-01-2026
+ * @par Revision history
+ * | Version |    Date    | Description                                                             |
+ * |---------|------------|-------------------------------------------------------------------------|
+ * | 0.1     | 13-01-2026 | Creaded file,                                                           |
+ * | 0.1     |            |                                                                         |
+ * 
+ * @copyright Copyright (c) 2026
+ */
+
 #ifndef LOGIN_HTML_H
 #define LOGIN_HTML_H
 
