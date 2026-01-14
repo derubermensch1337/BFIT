b4eed4203ab9bc8ac1fd0d310038c43df0b5afa9
s234384 <155777444+s234384@users.noreply.github.com>
2026-01-14 14:16:50 +0100

    Update inventory.h
    Added documentation

--- include/inventory.h
+++ include/inventory.h
@@ -1,3 +1,19 @@
+/**
+ * @file inventory.h
+ * @authors Baldur G. Toftegaard,
+ * @brief  
+ * @date 14-01-2026
+ * @version 0.1
+ * @par Revision history
+ * | Version |    Date    | Description                       |
+ * |---------|------------|-----------------------------------|
+ * | 0.1     | 14-01-2026 | Create file                       |
+ * | 0.1     |            |                                   |
+ *
+ * @copyright Copyright (c) 2026
+ * 
+*/
+
 #ifndef INVENTORY_H
 #define INVENTORY_H
 
