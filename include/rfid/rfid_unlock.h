0ccdcc4ee2b95ea378a485d227cc06f9e8f771ec
Amal Araweelo Almis <amal-araweelo@github.com>
2026-01-15 11:13:12 +0100

    outline for add_room_number

--- include/rfid/rfid_unlock.h
+++ include/rfid/rfid_unlock.h
@@ -19,5 +19,51 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
+#define MAX_ROOMS = 17
+#define UID_LENGTH = 4 // 4 bytes
+
+// User struct containing room number and UID
+struct User {
+    byte uid[UID_LENGHT];
+    int roomNumber;
+}
+
+// Create all users
+User user[MAX_ROOMS];
+int userCount = 0;
+
+
+// Add room number 
+void add_new_room_number (){
+    if (userCount >= MAX_ROOMS) {
+        Serial.println("Database full.");
+        return;
+    }   
+
+    // prompt user for their room number
+
+
+    // read tag
+
+    // check if it already exists by comparing UIDs
+
+        // if it does, print "already in database"
+        // if not, add to database and print something like "resident added"
+
+}
+
+
+
+// Helper functions
+// Compares two uid tags and returns true if they are identical
+bool compare_UID (byte *uid1, byte *uid2){
+    for (int i=0; i<UID_LENGTH;i++){
+        if (uid1[i] != uid2[i]){
+            return false;
+        } 
+    }
+    return true; 
+}
+
 
 #endif 
