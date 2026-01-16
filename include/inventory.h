8327b71ef7507597912403cb2615896be7215b9f
Baldur Gunnarsson Toftegaard <s234384@dtu.dk>
2026-01-16 11:38:32 +0100

    Fixed naming and comments
    Changes to be committed:
    	modified:   include/inventory.h
    	modified:   src/inventory.cpp

--- include/inventory.h
+++ include/inventory.h
@@ -7,36 +7,37 @@
 #ifndef INVENTORY_H
 #define INVENTORY_H
 
-#include "Arduino.h"
+#include <stdint.h>
+#include <stdbool.h>
 
 #define INVENTORY_CAPACITY 6
 
 /** 
  * @brief Types of beverages supported by the system. 
 */
-typedef enum {                  // Create the different types of beverages.
-    beer,               /**< Beer */
-    cider,              /**< Cider */
-    soda,               /**< Soda */
-    limfjords_porter,   /**< Limfjords porter */
-    other               /**< Other */
-} item_type;
+typedef enum {
+    beer,                   /**< Beer */
+    cider,                  /**< Cider */
+    soda,                   /**< Soda */
+    limfjords_porter,       /**< Limfjords porter */
+    other                   /**< Other */
+} beverage_type;
 
 /** 
  * @brief Struct holding the information for one item.
 */
 typedef struct {
-    char name[20];      /**< Beverage  name */              
-    item_type item;     /**< Beverage  type */
-    uint8_t weight;     /**< Beverage  weight */
-    uint8_t price;      /**< Beverage  price */
-} item;
+    char name[20];          /**< Beverage  name */              
+    beverage_type beverage; /**< Beverage  type */
+    uint8_t weight;         /**< Beverage  weight */
+    uint8_t price;          /**< Beverage  price */
+} product;
 
 /**
  * @brief Struckt for keeping track of the original and current quantity of a beverage.
 */
 typedef struct {
-    item product;           /**< The beverage being tracked */
+    product beverage;       /**< The beverage being tracked */
     uint8_t original_qty;   /**< The original quantity of the beverage in stock */
     uint8_t current_qty;    /**< The current quantity of the beverage in stock */
 } products_stocked;
@@ -45,15 +46,15 @@ typedef struct {
  * @brief Struckt for holding the fridge inventory.
 */
 typedef struct {
-    products_stocked fridge[6];         /**< The beverages being stocked */
-    uint8_t number_products_stocked;    /**< Number of beverages stocked, prevents overflow */
+    products_stocked fridge[INVENTORY_CAPACITY];    /**< The beverages being stocked */
+    uint8_t number_products_stocked;                /**< Number of beverages stocked, prevents overflow */
 } inventory;
 
 
 /**
  * @brief Function for initualicing the inventory
  * 
- * @param inventory 
+ * @param Inventory Inventory instance to initialize 
  */
 void inventory_init(inventory *inventory);
 
@@ -64,19 +65,19 @@ void inventory_init(inventory *inventory);
  * @param type 
  * @param weight 
  * @param price 
- * @return item 
+ * @return item created
  */
-item make_item(const char *name, item_type type, uint8_t weight, uint8_t price);
+product make_product(const char *name, beverage_type type, uint8_t weight, uint8_t price);
 
 /**
  * @brief Function for adding product to inventory.
  * 
  * @param inventory 
- * @param product 
+ * @param beverage 
  * @param quantity 
- * @return true 
- * @return false 
+ * @return true - the beverage was added to the inventory
+ * @return false - there was an error adding the beverage
  */
-bool inventory_add_product(inventory *inventory, item product, uint8_t quantity);
+bool inventory_add_product(inventory *inventory, product beverage, uint8_t quantity);
 
 #endif
--- src/inventory.cpp
+++ src/inventory.cpp
@@ -6,38 +6,37 @@
 
 #include "inventory.h"
 #include <string.h>
-#include <stdbool.h>
 
-void inventory_init(inventory *inventory) {                         // make an instance of the inventory.
-    memset(inventory, 0, sizeof(*inventory));                       // make sure that the part of memory used for storing beverages is empty befor we start filling inventory.
+void inventory_init(inventory *inventory) {                             // make an instance of the inventory.
+    memset(inventory, 0, sizeof(*inventory));                           // make sure that the part of memory used for storing beverages is empty befor we start filling inventory.
 }
 
-item make_item(const char *name, item_type type, uint8_t weight, uint8_t price) {
-    item new_item;                                                  // we make an instance of the item type.
-    memset(&new_item, 0, sizeof(new_item));                         // we start by making sure that the memory used for the item is empty.
+product make_product(const char *name, beverage_type type, uint8_t weight, uint8_t price) {
+    product new_product;                                                // we make an instance of the item type.
+    memset(&new_product, 0, sizeof(new_product));                       // we start by making sure that the memory used for the item is empty.
 
-    if (name) {                                                     // make sure the user provided an input for the name.
-        strncpy(new_item.name, name, sizeof(new_item.name) - 1);    // copy the input into the instanse of the item strukt.
-        new_item.name[sizeof(new_item.name) - 1] = '\0';            // go back and manualy add a end of line terminator (incase the input was to long and got cut of).
+    if (name) {                                                         // make sure the user provided an input for the name.
+        strncpy(new_product.name, name, sizeof(new_product.name) - 1);  // copy the input into the instanse of the item strukt.
+        new_product.name[sizeof(new_product.name) - 1] = '\0';          // go back and manualy add a end of line terminator (incase the input was to long and got cut of).
     } 
 
-    new_item.item = type;                                           // we give the item a type.
-    new_item.weight = weight;                                       // we give the item a weight. 
-    new_item.price = price;                                         // we give the item a price.
-    return new_item;
+    new_product.beverage = type;                                        // we give the item a type.
+    new_product.weight = weight;                                        // we give the item a weight. 
+    new_product.price = price;                                          // we give the item a price.
+    return new_product;
 }
 
-bool inventory_add_product(inventory *inventory, item product, uint8_t quantity) {
-    if (inventory->number_products_stocked >= INVENTORY_CAPACITY)   // check if the user has atempted to add more products than that suported by the code. If so return false. 
+bool inventory_add_product(inventory *inventory, product product, uint8_t quantity) {
+    if (inventory->number_products_stocked >= INVENTORY_CAPACITY)       // check if the user has atempted to add more products than that suported by the code. If so return false. 
         return false;  
 
     products_stocked *product_in_fridge = &inventory->fridge[
-        inventory->number_products_stocked];                        // we make an pointer into the product_stock struct of the inventory.
+        inventory->number_products_stocked];                            // we make an pointer into the product_stock struct of the inventory.
     
-    product_in_fridge->product = product;                           // we can now use that pointer to add the product.
-    product_in_fridge->original_qty = quantity;                     // add the starting stock quantity,
-    product_in_fridge->current_qty  = quantity;                     // add the current stock quantity.
+    product_in_fridge->product = product;                               // we can now use that pointer to add the product.
+    product_in_fridge->original_qty = quantity;                         // add the starting stock quantity,
+    product_in_fridge->current_qty  = quantity;                         // add the current stock quantity.
 
-    inventory->number_products_stocked++;                           // increase the counter keeping track of the number of different products stocked.
-    return true;                                                    // if adding the priduct was succsesfull we return 1
+    inventory->number_products_stocked++;                               // increase the counter keeping track of the number of different products stocked.
+    return true;                                                        // if adding the priduct was succsesfull we return 1
 }
