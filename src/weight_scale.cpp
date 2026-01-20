67dd42a05b7f75af4abc0ccb4704f2cfd96a8a40
Amal Araweelo Almis <amal-araweelo@github.com>
2026-01-20 11:51:07 +0100

    init weight scale header

---
+++ src/weight_scale.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file weight_scale.cpp
+ * @authors Amal Araweelo Almis
+ * @brief 
+ * @version 0.1
+ * @date 20-01-2026
+ * @par Revision history
+ * | Version |    Date    | Description                                                             |
+ * |---------|------------|-------------------------------------------------------------------------|
+ * | 0.1     | 20-01-2026 | Created file.                                                           |
+ * |         |            |                                                                         |
+ * 
+ * @copyright Copyright (c) 2026
+ */
