35f3fe57ae896b36b621ab90c9f44246164d0272
Baldur Gunnarsson Toftegaard <s234384@dtu.dk>
2026-01-23 16:04:09 +0100

    updated commentary

--- include/rfid_access.h
+++ include/rfid_access.h
@@ -19,7 +19,9 @@
 #define MAX_ROOMS 17
 #define UID_LENGTH 4
 
-// Commands
+/**
+ * @brief Supported serial commands for the RFID management interface.
+*/
 enum RFIDcommand {
     CMD_NONE,
     CMD_ADD_USER,
@@ -30,43 +32,199 @@ enum RFIDcommand {
     CMD_PRINT
 };
 
-// User struct
+/**
+* @brief User record stored in the RFID user database.
+*/
 struct User {
     byte uid[UID_LENGTH];
     int roomNumber;
     int balance; 
 };
 
-// Extern globals declared
+/**
+ * @brief Global user database array.
+*/
 extern User users[MAX_ROOMS];
+
+/**
+ * @brief Number of currently registered users.
+*/
 extern int userCount;
 
-// Function declarations
-RFIDcommand check_command(void);
-void setup_RFID_reader(MFRC522 &rfid);
-bool add_user(MFRC522 &rfid);
-bool remove_user();
-bool compare_UID(byte *uid1, byte *uid2);
-bool read_RFID_tag(MFRC522 &rfid, byte *uidBuffer);
-void display_commands(void);
-void display_commands_um();
+/**
+ * @brief brief Reads a command from the serial interface and maps it to an RFIDcommand.
+ * 
+ * @return RFIDcommand 
+*/
+RFIDcommand check_command(
+    void
+);
 
+/**
+ * @brief Initializes SPI and the MFRC522 RFID reader.
+ * 
+ * @param rfid 
+*/
+void setup_RFID_reader(
+    MFRC522 &rfid
+);
 
-//Anssi
-void get_users_db(User* ptr);
-void user_management(RFIDcommand cmd, User* ptr, MFRC522 &rfid);
-bool validate_rfid(MFRC522 myRFID);
+/**
+ * @brief Adds a new user by reading room number and scanning an RFID tag.
+ * 
+ * @param rfid 
+ * @return true 
+ * @return false 
+*/
+bool add_user(
+    MFRC522 &rfid
+);
 
-//Printing users
-void print_single_user(User* ptr, int idx);
-void print_all_users(User* ptr);
-void print_uid(byte* ptr);
+/**
+ * @brief Removes a user from the database.
+ * 
+ * @return true 
+ * @return false 
+*/
+bool remove_user(
+);
 
-// Helpers
-int read_integer();
-bool read_confirmation();
-int find_empty_index(User* ptr);
-int count_rooms(User* ptr);
+/**
+ * @brief 
+ * 
+ * @param uid1 
+ * @param uid2 
+ * @return true 
+ * @return false 
+*/
+bool compare_UID(
+    byte *uid1, 
+    byte *uid2
+);
+
+/**
+ * @brief 
+ * 
+ * @param rfid 
+ * @param uidBuffer 
+ * @return true 
+ * @return false 
+*/
+bool read_RFID_tag(
+    MFRC522 &rfid, 
+    byte *uidBuffer
+);
+
+/**
+ * @brief 
+*/
+void display_commands(
+    void
+);
+
+/**
+ * @brief 
+*/
+void display_commands_um(
+);
+
+/**
+ * @brief Get the users db object
+ * 
+ * @param ptr 
+*/
+void get_users_db(
+    User* ptr
+);
+
+/**
+ * @brief 
+ * 
+ * @param cmd 
+ * @param ptr 
+ * @param rfid 
+*/
+void user_management(
+    RFIDcommand cmd, 
+    User* ptr, 
+    MFRC522 &rfid
+);
+
+/**
+ * @brief 
+ * 
+ * @param myRFID 
+ * @return true 
+ * @return false 
+*/
+bool validate_rfid(
+    MFRC522 myRFID
+);
+
+/**
+ * @brief 
+ * 
+ * @param ptr 
+ * @param idx 
+*/
+void print_single_user(
+    User* ptr, 
+    int idx
+);
+/**
+ * @brief 
+ * 
+ * @param ptr 
+*/
+void print_all_users(
+    User* ptr
+);
+
+/**
+ * @brief 
+ * 
+ * @param ptr 
+*/
+void print_uid(
+    byte* ptr
+);
+
+/**
+ * @brief 
+ * 
+ * @return int 
+*/
+int read_integer(
+);
+
+/**
+ * @brief 
+ * 
+ * @return true 
+ * @return false 
+*/
+bool read_confirmation(
+);
+
+/**
+ * @brief 
+ * 
+ * @param ptr 
+ * @return int 
+*/
+int find_empty_index(
+    User* ptr
+);
+
+/**
+ * @brief 
+ * 
+ * @param ptr 
+ * @return int 
+*/
+int count_rooms(
+    User* ptr
+);
 
 /**
  * @brief Function for storing the last used RFID
@@ -84,7 +242,7 @@ void rfid_set_last_uid(
  * @return true 
  * @return false 
 */
-bool rfid_get_last_uid(
+bool rfid_get_last_uid( 
     byte *uidOut
 );
 
